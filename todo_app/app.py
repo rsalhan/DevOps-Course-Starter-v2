600948e2ad59e2e5baae2e2415226459acd80743
Rajeev Salhan <rajeev.salhan@gmail.com>
2023-06-28 02:22:33 +0100

    second commit

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+#import todo_app.data.session_items as module_SI
+from todo_app.data.session_items import get_items, add_item
 
+from flask import Flask, render_template, request, redirect
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -8,4 +10,12 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    #retrieve_items = module_SI.get_items()
+    retrieve_items = get_items()
+    return render_template('index.html', items_list = retrieve_items)
+
+@app.route('/additem', methods = ["POST"])
+def add_new_item():
+    new_todo = request.form.get('new-todo')
+    add_item(new_todo)
+    return redirect('/')

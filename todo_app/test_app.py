c64e82c44fcbe38b993ccf01bab8f85a4a63e3d3
Rajeev Salhan <rajeev.salhan@gmail.com>
2024-04-16 01:41:11 +0100

    16/04/2024 @01:41 modified:   todo_app/test_app.py

--- todo_app/test_app.py
+++ todo_app/test_app.py
@@ -10,12 +10,16 @@ def client():
     load_dotenv(file_path, override=True)
 
     with mongomock.patch(servers=(('fakemongo.com', 27017),)):
-    # Create the new app.
         test_app = app.create_app()
-
-    # Use the app to create a test_client that can be used in our tests.
         with test_app.test_client() as client:
             yield client
+
+    # # Create the new app.
+    # test_app = app.create_app()
+
+    # # Use the app to create a test_client that can be used in our tests.
+    # with test_app.test_client() as client:
+    #     yield client
 
 # class StubResponse():
 #     def __init__(self, fake_response_data):
@@ -24,7 +28,7 @@ def client():
 #     def json(self):
 #         return self.fake_response_data
     
-# Stub replacement for requests.get(url)
+# # Stub replacement for requests.get(url)
 # def stub(action, url, headers={}, params={}):
 #     test_todo_id = os.environ.get('TODO_IDLIST')
 #     test_doing_id = os.environ.get('DOING_IDLIST')
@@ -56,28 +60,18 @@ def client():
     # Replace requests.get(url) with our own function
     # monkeypatch.setattr(requests, 'request', stub)
 
-def test_index_page(monkeypatch: pytest.MonkeyPatch, client: FlaskClient):
-    # mongo_db_connection = os.getenv("MONGOMOCK_DB_CONNECTION_STRING")
-    # mongo_db_name = os.getenv("MONGOMOCK_DB_NAME")
+# def test_index_page(monkeypatch, client):
+# def test_index_page(monkeypatch: pytest.MonkeyPatch, client: FlaskClient):
+def test_index_page(client):
+    # mongo_db_connection = os.getenv("MONGO_DB_CONNECTION_STRING")
+    # mongo_db_name = os.getenv("MONGO_DB_NAME")
     # client = pymongo.MongoClient(mongo_db_connection)
     # db = client[mongo_db_name]
     # collection = db["collection1"]
-    # list_collections = db.list_collection_names()
-
-    # todo_1 = {
-    #     "name": "Module 11",
-    #     "status": "To Do"
-    # }
-
-    # todo_2 = {
-    #     "name": "Module 10",
-    #     "status": "Doing"
-    # }
-
-    # todo_3 = {
-    #     "name": "Module 9",
-    #     "status": "Done"
-    # }
+    
+    todo_1 = {"name": "Module 11", "status": "To Do"}
+    todo_2 = {"name": "Module 10", "status": "Doing"}
+    todo_3 = {"name": "Module 09", "status": "Done"}
 
     # collection.insert_many([todo_1, todo_2, todo_3])
 
@@ -87,4 +81,5 @@ def test_index_page(monkeypatch: pytest.MonkeyPatch, client: FlaskClient):
     assert response.status_code == 200
     # assert 'ToDo test card 2' in response.data.decode()
     # assert 'Module 11' in response.data.decode()
+    # assert 'Module 11' in response.data
 
